0bc43b972fbffaeafbea2f60c297b2b8a625752b
hugooom <hugo.montiege.cours@gmail.com>
2025-10-09 17:37:14 +0200

    Postgresql : Correction du dernier commit de @EvanChr

--- postgresql/readme.sh
+++ postgresql/readme.sh
@@ -1,5 +1,6 @@
 sudo apt update
+sudo apt upgrade
 sudo apt install -y postgresql postgresql-contrib
 sudo -u postgres psql -f postgresql/schema/script_init.sql
-psql -U blog_user -d blog_db -h localhost
-mot de passe postgre : postgresql
+sudo psql -U blog_user -d blog_db -h localhost
+# mot de passe postgre : postgresql
